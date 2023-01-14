f3ef937a24d9eaf02305af66f8a63350475fc988
Samia Ahmed <smim191137@bscse.uiu.ac.bd>
2023-01-14 12:36:36 +0600

    post details added

--- foodapp/src/screens/postfood.js
+++ foodapp/src/screens/postfood.js
@@ -19,7 +19,6 @@ const postfood = (props) => {
   const [foodcost, setFoodcost] = useState("");
   const [foodweight, setFoodweight] = useState("");
   const [contact, setContact] = useState("");
-
   const [location, setLocation] = useState("");
 
   const Create = () => {
@@ -210,134 +209,7 @@ borderWidth:2,
 
 export default postfood;
 
-// import { deleteDoc, doc, getDoc, setDoc } from 'firebase/firestore';
-// import { useState } from 'react';
-// import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-// // Using DB Reference
-// import { db } from '../screens/config'
 
-// export default function Cruds() {
 
-//   // Storing User Data
-//   const [userDoc, setUserDoc] = useState(null)
-//   // Update Text
-//   const [text, setText] = useState("")
 
-//   // MARK: CRUD Functions
-//   const Create = () => {
-//     // MARK: Creating New Doc in Firebase
-//     // Before that enable Firebase in Firebase Console
-//     const myDoc = doc(db, "Bikes", "MyBikes")
 
-//     // Your Document Goes Here
-//     const docData = {
-//       "name": "iJustine",
-//       "bio": "YouTuber"
-//     }
-
-//     setDoc(myDoc, docData)
-//       // Handling Promises
-//       .then(() => {
-//         // MARK: Success
-//         alert("Document Created!")
-//       })
-//       .catch((error) => {
-//         // MARK: Failure
-//         alert(error.message)
-//       })
-//   }
-
-//   const Read = () => {
-//     // MARK: Reading Doc
-//     // You can read what ever document by changing the collection and document path here
-//     const myDoc = doc(db, "Bikes","MyBikes")
-
-//     getDoc(myDoc)
-//       // Handling Promises
-//       .then((snapshot) => {
-//         // MARK: Success
-//         if (snapshot.exists) {
-//           setUserDoc(snapshot.data())
-//         }
-//         else {
-//           alert("No Doc Found")
-//         }
-//       })
-//       .catch((error) => {
-//         // MARK: Failure
-//         alert(error.message)
-//       })
-
-//   }
-
-//   const Update = (value, merge) => {
-//     // MARK: Updating Doc
-//     const myDoc = doc(db, "Bikes", "MyBikes")
-
-//     // If you set merge true then it will merge with existing doc otherwise it will be a fresh one
-//     setDoc(myDoc, value, { merge: merge })
-//       // Handling Promises
-//       .then(() => {
-//         // MARK: Success
-//         alert("Updated Successfully!")
-//         setText("")
-//       })
-//       .catch((error) => {
-//         // MARK: Failure
-//         alert(error.message)
-//       })
-//   }
-
-//   const Delete = () => {
-//     // MARK: Deleting Doc
-//     const myDoc = doc(db, "Bikes", "MyBikes")
-
-//     deleteDoc(myDoc)
-//       // Handling Promises
-//       .then(() => {
-//         // MARK: Success
-//         alert("Deleted Successfully!")
-//       })
-//       .catch((error) => {
-//         // MARK: Failure
-//         alert(error.message)
-//       })
-
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       <Button title='Create New Doc' onPress={Create}></Button>
-//       <Button title='Read Doc' onPress={Read}></Button>
-//       {
-//         userDoc != null &&
-//         <Text>Bio: {userDoc.bio}</Text>
-//       }
-//       <TextInput style={{
-//         width: '95%',
-//         fontSize: 18,
-//         padding: 12,
-//         borderColor: 'gray',
-//         borderWidth: 0.2,
-//         borderRadius: 10,
-//         marginVertical: 20
-//       }} placeholder='Type Here' onChangeText={(text) => { setText(text) }} value={text}></TextInput>
-
-//       <Button title='Update Doc' onPress={() => {
-//         Update({
-//           "bio": text
-//         }, true)
-//       }} disabled={text == ""}></Button>
-//       <Button title='Delete Doc' onPress={Delete}></Button>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
